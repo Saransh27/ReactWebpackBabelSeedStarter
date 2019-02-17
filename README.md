f3b841032760bea7a29390fa8c2af04c1bf89b2b
Saransh Mehra <saransh.mehra@mcmcg.com>
2019-02-17 13:30:38 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# ReactJS Webpack Babel Starter
+ReactJS Webpack Babel Starter
+
+## Getting Started
+
+```
+Please use an npm version that is >= 3.0.0 and < 5.0.0
+Much better install node.js version 5.11.1
+ReactJS version 16.8.1
+```
+### Installing
+```
+npm install
+npm start
+```
---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'core-js/es6/map';
+import 'core-js/es6/set';
+
+export default class App extends React.Component{
+    render(){
+        return(
+            <div className="test-class">
+                Hello from react!!!
+            </div>
+            )
+    }
+}
